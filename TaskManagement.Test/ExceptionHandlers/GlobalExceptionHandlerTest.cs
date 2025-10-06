52854c2c6b06b0fb1b8f47ce3ad76738d40ca179
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-10-06 21:19:47 +0300

    test: add global exception handler test

---
+++ TaskManagement.Test/ExceptionHandlers/GlobalExceptionHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TaskManagement.Api.Exceptions;
+
+namespace TaskManagement.Tests.ExceptionHandlers
+{
+    public class GlobalExceptionHandlerTest
+    {
+        private readonly Mock<ILogger<GlobalExceptionHandler>> logger;
+        private readonly Mock<IProblemDetailsService> problemDetailsService;
+        private readonly Mock<HttpContext> httpContext;
+        private readonly Mock<HttpResponse> response;
+        private readonly GlobalExceptionHandler handler;
+
+        public GlobalExceptionHandlerTest()
+        {
+            this.logger = new Mock<ILogger<GlobalExceptionHandler>>();
+            this.problemDetailsService = new Mock<IProblemDetailsService>();
+            this.httpContext = new Mock<HttpContext>();
+            this.response = new Mock<HttpResponse>();
+
+            handler = new GlobalExceptionHandler(logger.Object, problemDetailsService.Object);
+        }
+
+        [Fact]
+        public async Task TryHandleAsync_LogsException()
+        {
+            var exception = new InvalidOperationException();
+
+            problemDetailsService.Setup(x => x.TryWriteAsync(It.IsAny<ProblemDetailsContext>())).ReturnsAsync(true);
+
+            httpContext.Setup(x => x.Response.StatusCode).Returns(It.IsAny<int>());
+            response.Setup(x => x.ContentType).Returns(It.IsAny<string>());
+            await handler.TryHandleAsync(httpContext.Object , exception, default);
+
+            logger.Verify(logger => 
+            logger.Log(LogLevel.Error,
+            It.IsAny<EventId>() ,
+            It.Is<It.IsAnyType>((v, t) => true), exception , 
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        }
+    }
+}
